242e67ebc2f55bd35b278c0b25cea745166dc0c3
BuAleksandr <87057144+BuAleksandr@users.noreply.github.com>
2022-04-15 00:59:04 +1100

    Update Task 2_1.js

--- Task 2_1.js
+++ Task 2_1.js
@@ -1,19 +1,52 @@
-function num(number) {
-    for (let i = 2; i < number; i++) {
-        if (number % i === 0);
-        return false;
-    }
-    return number > 1;
+function isPrimeNumber(number) {
+  if (number < 2 ) {
+      return false;
+  }
+
+  if (number === 2) {
+      return true;
+  }
+
+  for(i = 2; i < Math.sqrt(number) + 1; ++i) {
+      if (number % i == 0) {
+          return false;
+      }
+  }
+
+  return true;
 }
 
-function numbers(number) {
-    const numb = [];
-    for (let i = 2; i <= number; i++) {
-        if (num(i)) {
-            numb.push(i);
-        }
-    }
-    return numb;
+
+function getPrimeNumbersList(nubmersCount) {
+  let result = [];
+  let number = 2;
+  while ( result.length < nubmersCount) {
+      if (isPrimeNumber(number)) {
+          result.push(number);
+      }
+      ++number;
+  }
+  return result;
 }
 
-console.log(numbers(100));
+
+function main() {
+  let nubmersCount = process.argv[2];
+
+  if(nubmersCount == undefined) {
+      console.log("undefined numbers count");
+      return;
+  }
+
+  nubmersCount = parseInt(nubmersCount)
+  if(isNaN(nubmersCount)) {
+      console.log("numbers count not number");
+      return;
+  }
+
+  primeNubmers = getPrimeNumbersList(nubmersCount);
+
+  console.log(primeNubmers);
+}
+
+main();
